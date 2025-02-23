3073fed5b8d0982f6bd4bb3545dbf9981f3ab488
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2025-02-24 01:55:36 +1100

    Added React Router

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Homepage from '@/pages/Homepage';
 import { Button } from './components/ui/button';
 import { MoonIcon } from 'lucide-react';
 import Social from './components/custom/Social';
+import { Routes, Route, Link } from 'react-router';
 
 function App() {
 	return (
@@ -10,10 +11,30 @@ function App() {
 			<div className='sticky top-0 z-50 bg-background/75 pt-6 backdrop-blur-sm'>
 				<nav className='w-full px-8 md:w-2xl h-10 mx-auto flex justify-between items-center md:px-0'>
 					<ul className='flex gap-4'>
-						<li>Home</li>
-						<li>Projects</li>
-						<li>Blog</li>
-						<li>Contact</li>
+						<Link to='/'>
+							<li className='text-muted-foreground hover:text-foreground cursor-pointer'>
+								Home
+							</li>
+						</Link>
+						<Link to='/projects'>
+							<li className='text-muted-foreground hover:text-foreground cursor-pointer'>
+								Projects
+							</li>
+						</Link>
+						<a
+							href='https://medium.com/@shanakaabeysinghe'
+							target='_blank'
+							rel='noreferrer noopener'
+						>
+							<li className='text-muted-foreground hover:text-foreground cursor-pointer'>
+								Blog
+							</li>
+						</a>
+						<Link to='/contact'>
+							<li className='text-muted-foreground hover:text-foreground cursor-pointer'>
+								Contact
+							</li>
+						</Link>
 					</ul>
 					<div>
 						{/* Theame selector */}
@@ -23,7 +44,11 @@ function App() {
 					</div>
 				</nav>
 			</div>
-			<Homepage />
+			<Routes>
+				<Route path='/' element={<Homepage />} />
+				<Route path='/projects' element={<h1>projects</h1>} />
+				<Route path='/contact' element={<h1>Contact</h1>} />
+			</Routes>
 			<footer className='flex flex-col pb-16 items-center justify-center sm:flex-row-reverse sm:justify-between w-full md:w-2xl mx-auto px-8 md:px-0'>
 				<div className='flex gap-4 mb-8 sm:mb-0'>
 					<Social />
